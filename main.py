37c6daa16af4de3e38bc49a967d85977654327ec
Nick Engle <enick11@gmail.com>
2019-05-14 13:50:18 -0400

    more basic work

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from abc import ABC
+
+
 # contains multiple rows, stored in a dict for speed.
 class Inventory:
     def __init__(self, rows={}):
@@ -26,18 +29,52 @@ class Row:
         
     def expand(self):
         self.size += 1
+        
+    def to_dict()
+    
+class SqlHandler:
+    def __init__(self, db_filename):
+        self.conn = sqlite3.connect(db_filename)
+        self.cursor = conn.cursor()
+        
+    def create_card(self, type, player):
+        pass
+    
+    def create_player(self, name, discord_id):
+        pass
+    
+    
     
 class BaseCard:
+    # should be able to recreate a card at any state
     def __init__(self, card_type):
         self.type = card_type
-        
+    
+    # For when card has an "action"
     def use(self):
         pass
-        
+    
+    # called as needed, use dt to update card state
     def passive(self, dt):
         pass
     
-    def 
+    # lossless
+    def to_dict(self):
+        pass
+    
+    # calls constructor, returns instance
+    def from_sql(**kwargs):
+        pass
+
+
+# makes 1 resource over time.
+# if instanciated, will make 10 money a hour up to 100.
+class SimplePassiveGenerator(BaseCard):
+    def __init__(self, resource='money', rate_per_hour=10, inventory_size=100, inventory_fill=0):
+        self.resource = resource
+        self.rate_per_hour = rate_per_hour
+        self.inventory_size = inventory_size
+        self.inventory_fill = inventory_fill
     
 class RowFilledError(Exception):
     """Raised when a card is inserted into a filled row"""
